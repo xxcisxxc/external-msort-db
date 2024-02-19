a7d2f49e48f861cd4eef55863d7cdaca95a492e6
xxcisxxc <32963789+xxcisxxc@users.noreply.github.com>
2024-02-19 00:24:29 +0000

    Add Record Class

--- Makefile
+++ Makefile
@@ -1,64 +1,73 @@
+.PHONY : all trace count clean test
+
 CPP=g++
-CPPOPT=-g -Og -D_DEBUG
-# -O2 -Os -Ofast
-# -fprofile-generate -fprofile-use
-CPPFLAGS=$(CPPOPT) -Wall -ansi -pedantic -std=c++17
-# -Wparentheses -Wno-unused-parameter -Wformat-security
-# -fno-rtti -std=c++11 -std=c++98
+CPPOPT=-g -Og # -D_DEBUG
+CPPFLAGS=$(CPPOPT) -Wall -Wextra -std=c++17
 
 # documents and scripts
 DOCS=Tasks.txt
 SCRS=
 
-.PHONY : all trace list count clean
-
 # default target
-all : Test.exe compile_commands.json
+all : Test.exe
 
 # headers and code sources
 HDRS=	defs.h \
-		Iterator.h Scan.h Filter.h Sort.h
-SRCS=	defs.cpp Assert.cpp Test.cpp \
+		Iterator.h Scan.h Filter.h Sort.h \
+		Record.h
+SRCS=	defs.cpp Assert.cpp \
 		Iterator.cpp Scan.cpp Filter.cpp Sort.cpp
 
 # compilation targets
-OBJS=	defs.o Assert.o Test.o \
+OBJS=	defs.o Assert.o \
 		Iterator.o Scan.o Filter.o Sort.o
 
-# generate compile_commands.json for clangd
-compile_commands.json : Makefile $(SRCS) $(HDRS)
-	@echo -n > compile_commands.json
-	@echo "[" >> compile_commands.json
-	@for file in $(SRCS) ; do \
-		if [ $$file = `echo $(SRCS) | awk '{print $$NF}'` ] ; then \
-			echo -n "  { \"directory\": \"`pwd`\", \"command\": \"$(CPP) $(CPPFLAGS) -c $$file\", \"file\": \"$$file\" }" >> compile_commands.json ; \
-		else \
-			echo -n "  { \"directory\": \"`pwd`\", \"command\": \"$(CPP) $(CPPFLAGS) -c $$file\", \"file\": \"$$file\" }," >> compile_commands.json ; \
-		fi ; \
-		echo >> compile_commands.json ; \
-	done
-	@echo "]" >> compile_commands.json
-
-
-Test.exe : Makefile $(OBJS)
-	$(CPP) $(CPPFLAGS) -o Test.exe $(OBJS)
+Test.exe : Makefile $(OBJS) Test.cpp $(HDRS)
+	$(CPP) $(CPPFLAGS) -o Test.exe Test.cpp $(OBJS)
 
 trace : Test.exe Makefile
 	@date > trace
 	./Test.exe >> trace
 	@size -t Test.exe $(OBJS) | sort -r >> trace
 
-$(OBJS) : Makefile defs.h
-Test.o : Iterator.h Scan.h Filter.h Sort.h
-Iterator.o Scan.o Filter.o Sort.o : Iterator.h
-Scan.o : Scan.h
-Filter.o : Filter.h
-Sort.o : Sort.h
+$(OBJS) : $(HDRS)
 
-list : Makefile
-	echo Makefile $(HDRS) $(SRCS) $(DOCS) $(SCRS) > list
-count : list
-	@wc `cat list`
+count :
+	@wc Makefile $(HDRS) $(SRCS) $(DOCS) $(SCRS) | sort -n
+
+TEST_DIR=tests
+TEST_SRCS=$(TEST_DIR)/test_record.cpp
+TEST_OBJS=$(TEST_SRCS:.cpp=.o)
+TEST_TARGETS=$(TEST_SRCS:.cpp=)
+TEST_LIBS=catch2/catch_amalgamated.o
+
+test : $(TEST_TARGETS)
+	@for test in $(TEST_TARGETS) ; do \
+		./$$test.out ; \
+	done
+
+$(TEST_TARGETS) : % : %.o $(TEST_LIBS) $(OBJS)
+	$(CPP) $(CPPFLAGS) -o $@.out $@.o $(TEST_LIBS) $(OBJS)
+
+$(TEST_OBJS) : catch2/catch_amalgamated.hpp $(HDRS) $(TEST_SRCS)
+	$(CPP) $(CPPFLAGS) -c -o $@ $*.cpp -I.
+
+$(TEST_LIBS) : catch2/catch_amalgamated.hpp
 
 clean :
-	@rm -f $(OBJS) Test.exe Test.exe.stackdump trace compile_commands.json
+	@rm -f $(OBJS) Test.exe Test.exe.stackdump trace
+	@rm -f $(TEST_OBJS) $(TEST_DIR)/test_record $(TEST_LIBS)
+
+# generate compile_commands.json for clangd
+# compile_commands.json : Makefile $(SRCS) $(HDRS)
+# 	@echo -n > compile_commands.json
+# 	@echo "[" >> compile_commands.json
+# 	@for file in $(SRCS) ; do \
+# 		if [ $$file = `echo $(SRCS) | awk '{print $$NF}'` ] ; then \
+# 			echo -n "  { \"directory\": \"`pwd`\", \"command\": \"$(CPP) $(CPPFLAGS) -c $$file\", \"file\": \"$$file\" }" >> compile_commands.json ; \
+# 		else \
+# 			echo -n "  { \"directory\": \"`pwd`\", \"command\": \"$(CPP) $(CPPFLAGS) -c $$file\", \"file\": \"$$file\" }," >> compile_commands.json ; \
+# 		fi ; \
+# 		echo >> compile_commands.json ; \
+# 	done
+# 	@echo "]" >> compile_commands.json
--- Record.h
+++ Record.h
@@ -1,19 +1,24 @@
 #pragma once
 
-#include <array>
-#include <bitset>
+#include <cstdio>
+#include <cstdlib>
+#include <memory>
+#include <new>
+#include <stdexcept>
 
-template <class Key = int, class Val = char, std::size_t Size = sizeof(Key)>
-struct Record {
-  static_assert(Size - sizeof(Key) >= 0);
+/**
+ * @brief A Record is a key-value pair.
+ *
+ * A variable-sized Record whose size is determined by static member \p bytes .
+ *
+ * @tparam Key The type of the key.
+ * @tparam Val The type of the value array.
+ */
+template <class Key = int, class Val = char> struct Record {
+  static inline std::size_t bytes = sizeof(Key);
 
-  union {
-    struct {
-      Key key;
-      std::array<Val, Size - sizeof(Key)> val;
-    };
-    std::bitset<Size * 8> bits;
-  };
+  Key key;
+  Val val[];
 
   inline bool operator<(Record const &rhs) const { return key < rhs.key; }
   inline bool operator>(Record const &rhs) const { return key > rhs.key; }
@@ -22,9 +27,83 @@ struct Record {
   inline bool operator<=(Record const &rhs) const { return key <= rhs.key; }
   inline bool operator>=(Record const &rhs) const { return key >= rhs.key; }
 
-  inline Record operator^(Record const &rhs) const {
-    Record result;
-    result.bits = bits ^ rhs.bits;
-    return result;
+  inline Record &x_or(Record const &rhs) {
+    if (bytes != rhs.bytes)
+      throw std::invalid_argument("Record sizes do not match");
+    key ^= rhs.key;
+    for (std::size_t i = 0; i < (bytes - sizeof(Key)) / sizeof(Val); ++i)
+      val[i] ^= rhs.val[i];
+    return *this;
   }
+
+  static inline void *operator new(std::size_t const size) {
+    if (sizeof(Record) > bytes || size != sizeof(Record))
+      throw std::bad_alloc();
+    return malloc(bytes);
+  }
+
+  static inline void *operator new[](std::size_t const size) {
+    if (size % sizeof(Record) != 0)
+      throw std::bad_alloc();
+    size_t const num_recs = size / sizeof(Record);
+    return malloc(num_recs * bytes);
+  }
+
+  static inline void operator delete(void *const ptr) { free(ptr); }
+
+  static inline void operator delete[](void *const ptr) { free(ptr); }
 };
+
+/**
+ * @brief Default Record type.
+ *
+ */
+using Record_t = Record<>;
+
+/**
+ * @brief A RecordArr is a wrapper for Record Array.
+ *
+ *
+ * @tparam Key The type of the key.
+ * @tparam Val The type of the value array.
+ */
+template <class Key = int, class Val = char> struct RecordArr {
+private:
+  using shared_arr = std::shared_ptr<Record<Key, Val>>;
+  shared_arr arr;
+
+public:
+  RecordArr() = default;
+  RecordArr(std::size_t const size) : arr(new Record<Key, Val>[size]) {}
+  ~RecordArr() = default;
+
+  RecordArr(const shared_arr &recs) : arr(recs) {}
+
+  RecordArr &operator=(const shared_arr &recs) {
+    arr = recs;
+    return *this;
+  }
+
+  Record<Key, Val> &operator[](std::size_t const idx) {
+    return *reinterpret_cast<Record<Key, Val> *>(
+        reinterpret_cast<char *>(arr.get()) + idx * Record<Key, Val>::bytes);
+  }
+  Record<Key, Val> const &operator[](std::size_t const idx) const {
+    return *reinterpret_cast<Record<Key, Val> *>(
+        reinterpret_cast<char *>(arr.get()) + idx * Record<Key, Val>::bytes);
+  }
+  // overload += operator
+  RecordArr &operator+=(std::size_t const count) {
+    arr = shared_arr(arr, reinterpret_cast<Record<Key, Val> *>(
+                              reinterpret_cast<char *>(arr.get()) +
+                              count * Record<Key, Val>::bytes));
+    return *this;
+  }
+  Record<Key, Val> *data() { return arr.get(); }
+};
+
+/**
+ * @brief Default RecordArr type.
+ *
+ */
+using RecordArr_t = RecordArr<>;
