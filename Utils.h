68be9b3e8f43bb4d67893727528bcb18db00e1db
xxcisxxc <32963789+xxcisxxc@users.noreply.github.com>
2024-04-21 02:03:19 +0000

    Format Utils.h

--- Utils.h
+++ Utils.h
@@ -4,7 +4,6 @@
 #include "Record.h"
 #include <cstddef>
 
-
 static inline std::size_t cache_nrecords() {
   return kCacheSize / (Record_t::bytes + sizeof(uint16_t));
 } // cache_nrecords
@@ -16,17 +15,16 @@ static inline std::shared_ptr<To> ptr_cast(std::shared_ptr<From> ptr) {
 
 // TODO::ranjitha - confirm if this is right
 static inline std::size_t dram_nrecords() {
-  //return kMemSize / (Record_t::bytes);
+  // return kMemSize / (Record_t::bytes);
   return 32;
-} 
+}
 
 static inline std::size_t cache_run_nrecords() {
-  //return kMemSize / (Record_t::bytes);
+  // return kMemSize / (Record_t::bytes);
   return 8;
-} 
+}
 
 static inline std::size_t ssd_nrecords() {
-  //return kSSDSize / (Record_t::bytes);
+  // return kSSDSize / (Record_t::bytes);
   return 256;
-} 
-
+}
